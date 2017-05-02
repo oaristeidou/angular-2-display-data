75e8ea781b357a7490c5c0c3394cbef5fa42e459
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-05-02 23:52:48 +0200

    - Showing component properties with interpolation. - Showing an array property with NgFor. - Conditional display with NgIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-display-data',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "Angular 2 member names";
+  members = ["Oddy", "Mohamed", "Thomas", "Tamara", "Felipe"];
+  person = this.members[0];
 }
